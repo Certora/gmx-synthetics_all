60cfb413e9789c1470744afe3d64e5ee151e41fa
hristo <hristo@certora.com>
2023-06-22 23:05:40 +0300

    basic setup for DepositVault

---
+++ certora/scripts/verifyDepositVault.sh
@@ -0,0 +1,19 @@
+certoraRun  contracts/deposit/DepositVault.sol \
+            contracts/role/RoleStore.sol \
+            contracts/data/DataStore.sol \
+            contracts/mock/WNT.sol \
+\
+--verify DepositVault:certora/specs/DepositVault.spec \
+\
+--link  DepositVault:dataStore=DataStore \
+        DepositVault:roleStore=RoleStore \
+--solc solc8.19 \
+--loop_iter 2 \
+--optimistic_loop \
+--packages @openzeppelin=node_modules/@openzeppelin \
+--solc_allow_path . \
+--staging \
+--rule_sanity \
+--prover_args "-optimisticFallback true" \
+--send_only \
+--msg "GMX DepositVault linking, dispatcher WNT" 
--- certora/scripts/verifyRoleStore.sh
+++ certora/scripts/verifyRoleStore.sh
@@ -1,6 +1,6 @@
 certoraRun ./contracts/role/RoleStore.sol \
 --verify RoleStore:certora/specs/RoleStore.spec \
---solc solc8.12 \
+--solc solc8.19 \
 --loop_iter 2 \
 --optimistic_loop \
 --packages @openzeppelin=node_modules/@openzeppelin \
@@ -9,4 +9,4 @@ certoraRun ./contracts/role/RoleStore.sol \
 --rule_sanity \
 --prover_args "-optimisticFallback true" \
 --send_only \
---msg "GMX RoleStore" 
+--msg "GMX RoleStore solc8.19" 
