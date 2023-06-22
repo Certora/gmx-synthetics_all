803c19aebb3c7bcfe0c8c55526dce8fdfc036cd2
hristo <hristo@certora.com>
2023-06-22 23:41:21 +0300

    basic setup for DataStore

---
+++ certora/scripts/verifyDataStore.sh
@@ -0,0 +1,17 @@
+certoraRun  contracts/data/DataStore.sol \
+            contracts/role/RoleStore.sol \
+\
+--verify DataStore:certora/specs/DataStore.spec \
+\
+--link  DataStore:roleStore=RoleStore \
+\
+--solc solc8.19 \
+--loop_iter 2 \
+--optimistic_loop \
+--packages @openzeppelin=node_modules/@openzeppelin \
+--solc_allow_path . \
+--staging \
+--rule_sanity \
+--prover_args "-optimisticFallback true" \
+--send_only \
+--msg "GMX DataStore linking" 
