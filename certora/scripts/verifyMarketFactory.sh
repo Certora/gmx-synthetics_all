a3fa6319022ac89b938af029fc8486304c120a1b
hristo <hristo@certora.com>
2023-06-23 21:43:03 +0300

    basic setup for MarketFactory

---
+++ certora/scripts/verifyMarketFactory.sh
@@ -0,0 +1,20 @@
+certoraRun  contracts/market/MarketFactory.sol \
+            contracts/role/RoleStore.sol \
+            contracts/data/DataStore.sol \
+            contracts/event/EventEmitter.sol \
+\
+--verify MarketFactory:certora/specs/MarketFactory.spec \
+\
+--link  MarketFactory:dataStore=DataStore \
+        MarketFactory:roleStore=RoleStore \
+        MarketFactory:eventEmitter=EventEmitter \
+--solc solc8.19 \
+--loop_iter 2 \
+--optimistic_loop \
+--packages @openzeppelin=node_modules/@openzeppelin prb-math=node_modules/prb-math \
+--solc_allow_path . \
+--staging \
+--rule_sanity \
+--prover_args "-optimisticFallback true" \
+--send_only \
+--msg "GMX MarketFactory linking munged to remove emits" 
