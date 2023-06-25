d8c59f3ec58d71c9d0b64995f10bc5da896fedfc
hristo <hristo@certora.com>
2023-06-25 10:23:56 +0300

    basic setup for MarketToken

---
+++ certora/scripts/verifyMarketToken.sh
@@ -0,0 +1,19 @@
+certoraRun  contracts/market/MarketToken.sol \
+            contracts/role/RoleStore.sol \
+            contracts/data/DataStore.sol \
+            contracts/mock/WNT.sol \
+\
+--verify MarketToken:certora/specs/MarketToken.spec \
+\
+--link  MarketToken:dataStore=DataStore \
+        MarketToken:roleStore=RoleStore \
+--solc solc8.19 \
+--loop_iter 2 \
+--optimistic_loop \
+--packages @openzeppelin=node_modules/@openzeppelin \
+--solc_allow_path . \
+--staging \
+--rule_sanity \
+--prover_args "-optimisticFallback true" \
+--send_only \
+--msg "GMX MarketToken linking, dispatcher WNT" 
