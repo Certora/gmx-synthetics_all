fd9e558a9e74aa30e520779e07e9e924d0e29aef
Andrew Ferraiuolo <andrew@certora.com>
2023-07-05 13:56:27 +0100

    non_controller_add_signer not reverting
    Now that I have updated this rule to also use the OracleStoreHarness
    alone and not the RoleStoreHarness, the assert about reverting fails...
    I get an error in the web UI that says "The Prover could not resolve the
    callee, thus, havoc'd the call":
    https://prover.certora.com/output/65266/6592847be32b4ff9951309544d30e319?anonymousKey=6c070378bd44a3c0096d1454f66518c809620ec7

--- certora/scripts/verifyOracleStore.sh
+++ certora/scripts/verifyOracleStore.sh
@@ -16,5 +16,5 @@ certoraRun  contracts/oracle/OracleStore.sol \
 --rule_sanity \
 --prover_args "-optimisticFallback true" \
 --send_only \
---msg "add_signer_valid_fix_harness" \
---rule add_signer_valid_liveness
+--rule non_controller_add_signer \
+--msg "non_controller_add_signer no rolestore harness"
