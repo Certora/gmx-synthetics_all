9caf4330e0fb6bd4262f8607f1ef781b5e4430fa
Andrew Ferraiuolo <andrew@certora.com>
2023-07-31 14:40:59 +0100

    Fix prover version

--- certora/scripts/verifyExchangeRouterSolvency.sh
+++ certora/scripts/verifyExchangeRouterSolvency.sh
@@ -15,11 +15,12 @@ certoraRun  contracts/router/ExchangeRouter.sol \
 --optimistic_loop \
 --packages @openzeppelin=node_modules/@openzeppelin prb-math=node_modules/prb-math \
 --solc_allow_path . \
---server production \
+--server staging \
+--prover_version master \
 --prover_args "-optimisticFallback true" \
 --prover_args "-splitParallel true" \
 --prover_args "-splitParallelTimelimit 1000" \
 --send_only \
 --rule positions_can_be_closed \
 --method "simulateExecuteOrder(bytes32,(address[],(uint256,uint256)[]))" \
---msg "positions_can_be_closed, parametric, simulateExecuteOrder"
+--msg "positions_can_be_closed, parametric, simulateExecuteOrder, using naftali fix (prover_version master)"
