83518fabdb95ed2c4646c649c69ac02d724f317b
Andrew Ferraiuolo <andrew@certora.com>
2023-10-26 13:44:23 +0100

    another commit to track a run variant that causes a crash

--- certora/scripts/verifyReqP1DecreaseOrder.sh
+++ certora/scripts/verifyReqP1DecreaseOrder.sh
@@ -15,10 +15,12 @@ certoraRun  certora/harness/DecreaseOrderUtilsHarness.sol \
 --packages @openzeppelin=node_modules/@openzeppelin prb-math=node_modules/prb-math \
 --solc_allow_path . \
 --server production \
---prover_version master
+--prover_version master \
 \
 --prover_args "-optimisticFallback true" \
 --prover_args '-copyLoopUnroll 1' \
 --prover_args "-solvers [z3]" \
+--prover_args "-depth 20, -mediumTimeout 60, -s\"[z3:def,z3:arith1,z3:lia1]\" " \
+--prover_args "-splitParallel -smt_parallelLIASolvers \"[z3:def,z3:arith1,z3:lia1]\" -smt_parallelNIASolvers [z3:def]" \
 --rule gmx_property1_DecreaseOrder_NoRevert \
---msg "DecreaseOrder version of ReqP1 using effect-based. simplify isPositionEmpty"
+--msg "DecreaseOrder version of ReqP1 using effect-based. simplify isPositionEmpty, test if merged fix works, use jaroslav suggested options"
